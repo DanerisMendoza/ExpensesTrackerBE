c6cb6846b0c50ef1206e8fd1477b2ac0004e0c21
Daneris Mendoza <danerismendoza096@gmail.com>
2025-01-02 22:37:44 +0800

    add dist to gitignore and add deploy command

--- dist/modules/user/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.permissionsTypes = void 0;
-var permissionsTypes;
-(function (permissionsTypes) {
-    permissionsTypes[permissionsTypes["admin"] = 0] = "admin";
-    permissionsTypes[permissionsTypes["endUser"] = 1] = "endUser";
-})(permissionsTypes || (exports.permissionsTypes = permissionsTypes = {}));
-//# sourceMappingURL=types.js.map

418f086f25ff20fd7bc340f9257c93cadce64020
Daneris Mendoza <danerismendoza096@gmail.com>
2025-01-11 07:21:54 +0800

    Update ExpensesSchema add some required field

--- api/modules/expenses/model.ts
+++ api/modules/expenses/model.ts
@@ -3,12 +3,28 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 const mongoDatabaseURL = process.env.MONGODB_URL;
 mongoose.connect(mongoDatabaseURL)
+
 const ExpensesSchema = new mongoose.Schema({
-    user_id: String,
-    title: String,
-    spent_at: Date,
-    amount: Number,
-}, { timestamps: true })
+    user_id: {
+        type: String,
+        required: true, 
+    },
+    title: {
+        type: String,
+        required: true, 
+        trim: true, 
+    },
+    spent_at: {
+        type: Date,
+        default: Date.now, 
+    },
+    amount: {
+        type: Number,
+        required: true,
+        min: [0, 'Amount cannot be negative'], 
+    },
+}, { timestamps: true });
+
 const ExpensesModel = mongoose.model("expenses", ExpensesSchema)
 
 export { ExpensesModel, ExpensesSchema };
